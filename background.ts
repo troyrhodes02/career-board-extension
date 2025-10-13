117a355eefc790ec94647445f30d3b1f3ce5ce81
Jadon Cardell <jcardell@MacBookPro.lan>
2025-10-13 18:14:10 -0500

    Background Service Worker

--- background.ts
+++
@@ -1,8 +0,0 @@
-console.log('Service Worker started');
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message === 'ping') {
-    console.log("Received 'ping' from popup");
-    sendResponse('pong');
-  }
-});
